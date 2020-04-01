ce90af20bc0d11c241d8215ee859aaaa48f27a9b
AndrejTsanda <tsanda.andrej@gmail.com>
2020-04-02 00:31:00 +0300

    #8: Simple Swagger UI added.

--- backend/entities/src/main/java/com/tsanda/entities/User.java
+++ backend/entities/src/main/java/com/tsanda/entities/User.java
@@ -1,17 +1,33 @@
 package com.tsanda.entities;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
+@ApiModel(description = "Model user for database.", value = "User Table")
 public class User {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "user_id")
+  @ApiModelProperty(
+      value = "The unique Id of the user more than zero.",
+      required = true,
+      position = 1,
+      example = "1",
+      hidden = true)
   private Integer id;
 
   @Column(name = "user_name")
+  @ApiModelProperty(
+      value = "Unique user name. Not empty value",
+      required = true,
+      position = 2,
+      example = "sidorov",
+      allowableValues = "range[1, 45]")
   private String userName;
 
   @Column(name = "user_password")
